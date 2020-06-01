681c785d3e034a10689551eaa7c4575fb3c34db6
simon <simoncoffey9@gmail.com>
2020-06-01 12:32:22 +0100

    registration form now has model and two way binding from generic Registraion object, some input vaildation,  no submit functionality yet

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RegistrationForm } from '../models/registration-form';
 
 @Component({
   selector: 'app-register',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+
+  registrationForm = new RegistrationForm('uname', 'email', 'password');
 
   constructor() { }
 
