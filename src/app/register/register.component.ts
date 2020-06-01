90f770675638645dab2d60831f5ca55a719ff11b
simon <simoncoffey9@gmail.com>
2020-06-01 14:29:02 +0100

    registration service added, commented out functionality that hits an actual endpoint

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RegistrationForm } from '../models/registration-form';
 import { NgForm } from '@angular/forms';
+import { RegistrationServiceService } from '../services/registration-service.service';
 
 @Component({
   selector: 'app-register',
@@ -10,16 +11,30 @@ import { NgForm } from '@angular/forms';
 export class RegisterComponent implements OnInit {
 
   registrationForm = new RegistrationForm('', '', '');
+  userSubmission;
 
-  constructor() { }
+  constructor(private _registrationService : RegistrationServiceService) { }
+
 
   ngOnInit(): void {
 
   }
 
   onSubmit(userForm:NgForm){
+  
     console.log(this.registrationForm)
     console.log(JSON.stringify(userForm.value))
+    this.userSubmission = new RegistrationForm(userForm.value.username, userForm.value.email,
+       userForm.value.password)
+    console.log(this.userSubmission)
+
+
+    //the below only works on my (simon's) setup using a node server to act as a fake
+    //back end 
+
+    // this._registrationService.register_new_user(this.userSubmission)
+    // .subscribe(data => console.log("whoop!", data),
+    // error => console.log("error", error))
 
   }
 
