642430c49a65b1adaa35e6bfff9351c512e3104f
simon <simoncoffey9@gmail.com>
2020-06-02 11:41:30 +0100

    models for Hotel, Room and Bookings added.  hard coded data at the moment as no url for a list of all hotels exists on back end

--- src/app/hotel-list/hotel-list.component.ts
+++ src/app/hotel-list/hotel-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Hotel } from '../models/hotel';
 
 @Component({
   selector: 'app-hotel-list',
@@ -18,12 +19,13 @@ export class HotelListComponent implements OnInit {
 
     // let backend_url = "link to backend url for hotels list";
     // let resp = this._http.get(backend_url);
-    // resp.subscribe(result => this.hotels = result as Hotel[])
+    // resp.subscribe(result => this.hotels = result as Hotel[],
+    //               error => console.log("hotel list GET call failed ", error))
 
-    this.hotels = [{"Name": "data", "otherData": "stuff"},
-                    {"Name":"the ritz", "otherData": "stuff"},
-                    {"Name":"the  balmoral", "otherData": "stuff"},
-                    {"Name":"the savoy", "otherData": "stuff"}
+    this.hotels = [{"hotelId":2,"hotelName":"Yotel","numOfRooms":50,"address":"some street","postcode":"EH71 7FA","city":"Edinburgh","ammenities":"bowling alley","bookings":[],"starRating":4,"room":[{"roomId":2,"roomType":"LUXURY","price":80.00,"roomTypeAndPrice":"LUXURY 80.00"},{"roomId":3,"roomType":"DELUXE","price":100.00,"roomTypeAndPrice":"DELUXE 100.00"}],"airportTransfers":true,"transferPrice":20,"verified":true},
+    {"hotelId":3,"hotelName":"Radisson Blue","numOfRooms":2,"address":"123 argyle street","postcode":"G3 6OP","city":"Glasgow","ammenities":"Conference Rooms, Bars, Near Central Station","bookings":[{"bookingId":3,"roomType":"STANDARD","hotel":"Radisson Blue","checkInDate":"2020-04-20","checkOutDate":"2020-04-30","roomPrice":60.00,"extrasPrice":0.00,"totalPrice":540.00,"extras":"NO_EXTRAS","checkInDateFormatted":"20/04/2020","checkOutDateFormatted":"30/04/2020"},{"bookingId":4,"roomType":"STANDARD","hotel":"Radisson Blue","checkInDate":"2020-04-20","checkOutDate":"2020-04-30","roomPrice":60.00,"extrasPrice":0.00,"totalPrice":540.00,"extras":"NO_EXTRAS","checkInDateFormatted":"20/04/2020","checkOutDateFormatted":"30/04/2020"}],"starRating":4,"room":[{"roomId":1,"roomType":"STANDARD","price":60.00,"roomTypeAndPrice":"STANDARD 60.00"}],"airportTransfers":false,"transferPrice":20,"verified":true},
+    {"hotelId":4,"hotelName":"Radisson Red","numOfRooms":43,"address":"456 argyle street","postcode":"G3 6RP","city":"Glasgow","ammenities":"Conference Rooms, Bars, Near Central Station","bookings":[],"starRating":4,"room":[{"roomId":1,"roomType":"STANDARD","price":60.00,"roomTypeAndPrice":"STANDARD 60.00"}],"airportTransfers":false,"transferPrice":20,"verified":false},
+    {"hotelId":1,"hotelName":"Travelodge Glasgow","numOfRooms":5,"address":"1 main street","postcode":"g43 6pq","city":"Glasgow","ammenities":"none","bookings":[],"starRating":3,"room":[],"airportTransfers":true,"transferPrice":20,"verified":true}
   ]
 
 }
--- src/app/models/hotel.ts
+++ src/app/models/hotel.ts
@@ -1,4 +1,20 @@
+import { Bookings } from './bookings';
+import { Room } from './room';
+
 export class Hotel {
-    Name:string;
-    otherData:string;
+    hotelId : number;
+    hotelName : string;
+    numOfRooms : number;
+    address : string;
+    postcode : string;
+    city : string;
+    ammenities : string;
+    bookings : Bookings[];
+    starRating : number;
+    room : Room[];
+    airportTransfers : boolean;
+    transferProce : number;
+    verified : boolean;
+    
 }
+
