d094293009008e052ee4d609fb63966424acf320
simon <simoncoffey9@gmail.com>
2020-06-02 13:43:35 +0100

    material design nav bar implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,14 @@ import { LoginComponent } from './login/login.component';
 import { LandingComponent } from './landing/landing.component';
 import { HotelListComponent } from './hotel-list/hotel-list.component';
 import { HotelItemComponent } from './hotel-list/hotel-item/hotel-item.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -18,13 +26,21 @@ import { HotelItemComponent } from './hotel-list/hotel-item/hotel-item.component
     LoginComponent,
     LandingComponent,
     HotelListComponent,
-    HotelItemComponent
+    HotelItemComponent,
+    MainNavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
