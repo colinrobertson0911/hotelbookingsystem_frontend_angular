79c3ef39e9677e94faf2a3808fcc3dc581ee5198
Jan Stanton <jan.stanton@fdmgroup.com>
2020-06-17 14:31:25 +0100

    Account HTML added user data

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+
 import {AuthenticationService} from '../services/authentication.service';
-import { StateService } from 'src/app/services/state.service';
-import { User } from '../models/user';
-import { Router } from '@angular/router';
+
 
 
 @Component({
@@ -12,24 +10,11 @@ import { Router } from '@angular/router';
   styleUrls: ['./account.component.css']
 })
 export class AccountComponent implements OnInit {
-  @Input() users : User[];
-
-  currentUser : User;
-
-  constructor(public authenticationService: AuthenticationService, private _http : HttpClient,
-    private stateService : StateService, private router : Router) { }
+ 
+  constructor(public authenticationService: AuthenticationService) { }
 
   ngOnInit(): void {
-    let allUsers = "http://localhost:8088/hotelbookingsystem/admin/AllUsers/";
-    let allUsersResp = this._http.get(allUsers);
-    allUsersResp.subscribe(allUsersResult => this.users = allUsersResult as User[],
-                  error => console.log("user list GET call failed ", error))
-
-  }
-
-  onClickToPage(id){
-    this.stateService.data = this.currentUser as User;
-    this.router.navigate([`/user/${id}`])
+   
     
     
   }
